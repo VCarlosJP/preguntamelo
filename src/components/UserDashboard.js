4a6f46d3d67bc9639eccecb11ed0a2bc30bb631c
VCarlosJP <carlosjp.villanueva@gmail.com>
2021-01-15 18:25:22 +0100

    Home/Login/SignUp/UserDashboard Components structure added

---
+++ src/components/UserDashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class UserDashboard extends React.Component {
+    render() {
+      return (
+        <div>
+            <h1>DashBoardComponent</h1>
+        </div>
+      );
+    }
+  }
