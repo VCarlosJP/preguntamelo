cbbed05fef1648912bf6505b881e2518c8ffe6f7
VCarlosJP <carlosjp.villanueva@gmail.com>
2021-01-17 19:32:50 +0100

    Username, login/logout button, ask question box added to UserDashBoard Component

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -4,7 +4,15 @@ export default class UserDashboard extends React.Component {
   render() {
     return (
       <div>
-        <h1>DashBoardComponent</h1>
+        <div className="header">
+          <h4 className="user-name">Carlos Villanueva</h4>
+          <button className="login-logout-button">Log Out</button>
+        </div>
+
+        <div className="ask-question-box">
+          <h1 className="ask-question-title">How can we help you?</h1>
+          <input className="search-box" placeholder="Ask a question..."></input>
+        </div>
       </div>
     );
   }
