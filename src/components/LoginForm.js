20dd3e5c11b1f00f40917b5c67ac70a8eb517709
jaimecuellar14 <jamescuellar14@gmail.com>
2021-01-18 10:20:55 +0100

    Continuacion desarrollo vista de login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,10 +2,21 @@ import React from 'react';
 import logo from "../logo.svg";
 
 
+
 export default class LoginForm extends React.Component{
+    
     constructor(props){
         super(props);
+        this.singIn = this.singIn.bind(this);
+        this.register = this.register.bind(this);
+    }
 
+    singIn(){
+        console.log("logeandose");
+    }
+    register(){
+        console.log(this.props);
+        //this.props.history.push("/signup");
     }
 
     render(){
@@ -22,6 +33,23 @@ export default class LoginForm extends React.Component{
                 <div className="login-form-text">
                     <div className="info-text">Login into your account</div>
                     <div className="info-text">Let's access to your dashboard</div>
+                </div>
+                <div className="login-form-section">
+                    <span>
+                        Email
+                    </span>
+                    <input placeholder="Email or Phone number"></input>
+                    <span>
+                        Password
+                    </span>
+                    <input placeholder="Password"></input>
+
+                    <a>Reset password</a>
+                    <button className="sign-in-button" onClick={this.singIn}>Sing in</button>
+                </div>
+                <div style={{display:'flex', alignItems:'center'}}>
+                    <p>Dont have an account yet?</p>
+                    <a style={{marginLeft:'10px', color:'rgb(191, 96, 29)', fontWeight:'bold'}} onClick={this.register}>Join our community</a>
                 </div>
             </div>
         )
