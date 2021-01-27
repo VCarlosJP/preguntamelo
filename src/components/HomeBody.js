b1ddafe9a1c61c8599c9d41db614922fc3318e09
jaimecuellar14 <jamescuellar14@gmail.com>
2021-01-27 10:58:17 +0100

    Agregamos seccion donde se muestran las categorias

--- src/components/HomeBody.js
+++ src/components/HomeBody.js
@@ -12,6 +12,11 @@ export default class HomeBody extends React.Component{
             {"id":2,"pregunta":"pregunta 2"},
             {"id":3,"pregunta":"pregunta 3"}
         ];
+        const categoriasMock = [
+            {"id":1,"nombre":"Lengua"},
+            {"id":2,"nombre":"Fisica"},
+            {"id":3,"nombre":"Informatica"}
+        ];
         return (
             <div className="home-body">
                 <div>
@@ -29,8 +34,17 @@ export default class HomeBody extends React.Component{
 
                 <div className="ultimas-preguntas">
                     {
-                        preguntasMock.map((item)=>{
-                            return (<li>{item.pregunta}</li>)
+                        preguntasMock.map((pregunta)=>{
+                            return (<li>{pregunta.pregunta}</li>)
+                        })
+                    }
+                </div>
+                <hr style={{width:"100%"}}/>
+                <div className="nuestras-categorias">
+                    <p>Nuestras categorias</p>
+                    {
+                        categoriasMock.map((categoria)=>{
+                            return(<li>{categoria.nombre}</li>)
                         })
                     }
                 </div>
