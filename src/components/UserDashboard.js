6b4899af33deb787377c5e3dcc901f080bc6f4e9
VCarlosJP <carlosjp.villanueva@gmail.com>
2021-01-27 19:41:31 +0100

    Question layout added. Buttons interaction added

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -2,8 +2,22 @@ import React from "react";
 
 import "./styles/UserDashboard.scss";
 
-export default class UserDashboard extends React.Component {
+// export default class UserDashboard extends React.Component {
+class UserDashboard extends React.Component {
+  state = {
+    questionButton: true,
+  };
+
+  showUserQuestions = () => {
+    this.setState({ questionButton: true });
+  };
+
+  showPublicQuestions = () => {
+    this.setState({ questionButton: false });
+  };
+
   render() {
+    let { questionButton } = this.state;
     return (
       <div>
         <div className="header">
@@ -27,17 +41,40 @@ export default class UserDashboard extends React.Component {
 
         <div className="select-mode-container">
           <div className="select-mode-box">
-            <button className="my-questions-mode-button">My Questions</button>
-            <button className="public-questions-mode-button">
+            <button
+              onClick={this.showUserQuestions}
+              className="my-questions-mode-button"
+              style={{ background: questionButton ? "#eeeeee" : "#ffffff" }}
+            >
+              My Questions
+            </button>
+            <button
+              onClick={this.showPublicQuestions}
+              className="public-questions-mode-button"
+              style={{ background: questionButton ? "#ffffff" : "#eeeeee" }}
+            >
               Public Questions
             </button>
           </div>
         </div>
 
         <div className="questions-main-container">
-          <div className="questions-main"></div>
+          <div className="questions-main">
+            <div className="question-element">
+              <a href="" className="question-title">
+                When I spot a possible improvement while reviewing, can I
+                suggest to join the paper as a co-author?
+              </a>
+              <br></br>
+              <a className="question-date">Asked 2 days ago</a>
+              <a className="question-status">Sent</a>
+              <hr></hr>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+export default UserDashboard;
